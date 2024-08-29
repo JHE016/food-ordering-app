099708863d5e1949bdb00179a1cc8c540d1131c7
JinjinHe <he.jinj@northeastern.edu>
2024-08-29 09:17:39 -0700

    Added protected route to user profile page

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,28 +1,33 @@
-import {Navigate, Route, Routes} from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/layout";
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
+import ProtectedRoute from "./auth/ProtectedRoute";
 
-const AppRoutes = ()=>{
-    return(
+const AppRoutes = () => {
+    return (
         <Routes>
             <Route path="/" element={
                 <Layout showHero>
-                <HomePage />
+                    <HomePage />
                 </Layout>
             }
             />
             <Route path="/auth-callback" element={<AuthCallbackPage />} />
-            <Route 
-                path="/user-profile" 
-                element=
-                  { <Layout>
-                     < UserProfilePage />
+
+            <Route element={<ProtectedRoute />}>
+                <Route
+                    path="/user-profile"
+                    element=
+                    {<Layout>
+                        < UserProfilePage />
                     </Layout>
-                  }
-            />
-            <Route path="*" element={<Navigate to="/"/>}/>
+                    }
+                />
+            </Route>
+
+            <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     )
 }
